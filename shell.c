f0c7d57535b14736a7d95d1f4909c26247012ebd
ryu0812 <50409143+ryu0812@users.noreply.github.com>
2019-05-26 16:39:13 +0900

    Add files via upload

--- shell.c
+++ shell.c
@@ -1,1 +1,82 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+
+#define MAX_LEN_LINE    10
+#define LEN_HOSTNAME	30
+
+int main(void)
+{
+    char command[MAX_LEN_LINE];
+    char *args[] = {command, NULL};
+    int ret, status;
+    pid_t pid, cpid;
+    
+    while (true) {
+        char *s;
+        int len;
+        
+ 	//host name
+	char hostname[LEN_HOSTNAME + 1];
+    	memset(hostname, 0x00, sizeof(hostname));
+	gethostname(hostname, LEN_HOSTNAME);
+	printf("%s@%s:$ ", getpwuid(getuid())->pw_name, hostname);
+
+
+        s = fgets(command, MAX_LEN_LINE, stdin);
+        if (s == NULL) {
+            fprintf(stderr, "fgets failed\n");
+            exit(1);
+        }
+        
+        len = strlen(command);
+        printf("%d\n", len);
+        if (command[len - 1] == '\n') {
+            command[len - 1] = '\0'; 
+        }
+        
+        printf("[%s]\n", command);
+	
+
+	//exit 
+	if(strcmp(s,"exit")==0){
+		break;
+	}
+
+	//a;b;c;
+	char* ptr = strtok(s,";");
+	
+	while(ptr){		//
+
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } 
+        if (pid != 0) {  /* parent */
+            cpid = waitpid(pid, &status, 0);
+            if (cpid != pid) {
+                fprintf(stderr, "waitpid failed\n");        
+            }
+            printf("Child process terminated\n");
+            if (WIFEXITED(status)) {
+                printf("Exit status is %d\n", WEXITSTATUS(status)); 
+            }
+        }
+        else {  /* child */
+            ret = execve(args[0], args, NULL);
+            if (ret < 0) {
+                fprintf(stderr, "execve failed\n");   
+                return 1;
+            }
+        }
+	ptr=strtok(NULL,";"); 
+	}
+    }
+    return 0;
+}
